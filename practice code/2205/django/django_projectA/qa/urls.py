822d35f2866ee21618c1a2d96b7b9d63df33d769
kimgyuhee <42735333+kimgyuhee@users.noreply.github.com>
2022-05-26 10:22:35 +0900

    페이지이동

--- practice code/2205/django/django_projectA/qa/urls.py
+++ practice code/2205/django/django_projectA/qa/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('main/', views.main, name='name'),
+    path('main/country.html', views.country, name='country'),
+    path('main/profile.html', views.profile, name='profile'),
+    path('main/quiz.html', views.quiz, name='quiz'),
 ]
